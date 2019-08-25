e56d8588f59571e88666d24e7acc7707aab4ca7d
Jari Tolkki <tolkkijari@gmail.com>
2019-08-25 13:04:39 +0300

    Update configs.js
    Deleted a wrong url

--- vehicledatavisualizer-client/src/configs.js
+++ vehicledatavisualizer-client/src/configs.js
@@ -1,5 +1,4 @@
-const frontEndUrl = 'http://35.228.185.14:8080';
-//const frontEndUrl = 'http://localhost:8080'
+const frontEndUrl = 'http://localhost:8080'
 
 module.exports = {
   frontEndUrl: frontEndUrl
